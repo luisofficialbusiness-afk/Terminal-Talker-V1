bb5864954ce65efae1d012901c79b611de36c897
RVEPRTY <luisofficialbusiness@gmail.com>
2026-01-07 17:06:29 -0600

    Update messages.js

--- api/messages.js
+++ api/messages.js
@@ -2,7 +2,7 @@ let messages = [];
 
 export default function handler(req, res) {
   const { room } = req.query;
-  if (!room) return res.status(400).json({ error: "Room missing" });
-  const roomMessages = messages.filter(m => m.room === room);
-  res.status(200).json(roomMessages.slice(-200));
+  if (!room) return res.status(400).json([]);
+
+  res.status(200).json(messages.filter(m => m.room === room));
 }
