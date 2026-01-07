e9a6134d2925219b379496e8d05f15b6615b4271
luisofficialbusiness-afk <luisofficialbusiness@gmail.com>
2026-01-07 08:24:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const input = document.getElementById("input");
 /* ---------------- CONFIG ---------------- */
 const WEBSITE_OWNER_PASS = "O#0363168";
 const WEBSITE_ADMIN_PASS = "admin01";
-const BETA_PASS = "BETA2026";
+const BETA_PASS = "BETAPROGRAM";
 const EARLY_ACCESS_OPEN = true;
 
 /* ---------------- STATE ---------------- */
