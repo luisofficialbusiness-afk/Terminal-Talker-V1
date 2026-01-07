5cacb013784f27fb15d3058cef6f67dd9fa35dac
luisofficialbusiness-afk <luisofficialbusiness@gmail.com>
2026-01-06 20:35:40 -0600

    Update messages.js

--- api/messages.js
+++ api/messages.js
@@ -1,16 +1,13 @@
-import { wss, rooms } from "./rooms";
+import { rooms, wss } from "./rooms";
 
-export default function handler(req, res){
+export default function handler(req,res){
   if(req.method==="POST"){
     const { room, user, message } = req.body;
     if(!rooms[room]) rooms[room]=[];
     rooms[room].push({user,message,time:Date.now()});
-
-    // Broadcast to all connected sockets
     wss.clients.forEach(client=>{
       if(client.readyState===1) client.send(JSON.stringify({room, content:`[${user}] > ${message}`}));
     });
-
     res.status(200).json({ok:true});
   } else res.status(405).json({error:"Method not allowed"});
 }
