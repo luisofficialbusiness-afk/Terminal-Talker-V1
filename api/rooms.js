f08a3c1fc29b2939fb582fcea7e9fba505a3e697
luisofficialbusiness-afk <luisofficialbusiness@gmail.com>
2026-01-07 08:09:37 -0600

    Update rooms.js

--- api/rooms.js
+++ api/rooms.js
@@ -1,37 +1,5 @@
-import { WebSocketServer } from "ws";
+let rooms = ["general"]; // default room
 
-const rooms = {};
-const wss = new WebSocketServer({ noServer: true });
-
-wss.on("connection", socket => {
-  socket.on("message", msg => {
-    const data = JSON.parse(msg);
-
-    if (data.action === "resetServer") {
-      for (const r in rooms) rooms[r] = [];
-      wss.clients.forEach(c =>
-        c.send(JSON.stringify({ room: "general", content: "[SYSTEM] Server reset" }))
-      );
-      return;
-    }
-
-    if (data.action === "join") {
-      rooms[data.room] ??= [];
-      socket.send(JSON.stringify({ room: data.room, content: `Joined ${data.room}` }));
-      return;
-    }
-
-    if (data.action === "say") {
-      rooms[data.room] ??= [];
-      rooms[data.room].push(data);
-      wss.clients.forEach(c =>
-        c.send(JSON.stringify({
-          room: data.room,
-          content: `[${data.user}] > ${data.message}`
-        }))
-      );
-    }
-  });
-});
-
-export default wss;
+export default function handler(req, res) {
+  res.status(200).json(rooms);
+}
